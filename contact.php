67b9b7bdd3ba262739fbb422101eb19c637e4af6
Yanick Lafontaine <yanicklafontaine@hotmail.com>
2016-07-28 13:42:12 -0400

    Add some files

---
+++ contact.php
@@ -0,0 +1,80 @@
+<?php
+include 'sections/header.php';
+include 'sections/menu.php';
+include 'sections/banner.php';
+?>
+
+
+    <!-- OP:CONTENT -->
+    <div id="content">
+
+        <?php
+        include 'sections/left.php';
+        ?>
+
+        <!-- OP:MIDDLE COLUMN -->
+        <div id="middle">
+            <h2>Main Content</h2>
+            <div class="main">
+                <div class="inner">
+                    <img src="images/main_cont_bg_top.gif" alt=""/>
+                    <!-- INSERT MAIN CONTENT HERE -->
+                    <h3>Intro</h3>
+                    <p>Notre communauté Teamspeak est ouverte à tous ceux et celle qui veulent jouer à des jeux sur ordinateur entre amis.
+                        Nous disposons actuellement d'une équipe dynamique qui ç'aura vous divertir sans problème.
+                        Les fondateurs du serveur se feront un plaisir d'ajouter des canaux en fonction de vos jeux et habitude de "gaming".
+                        La musique et l'animation sont au rendez-vous avec nos Bot Music. Rejoignez-nous sans tarder.
+                        Vous avez même la possibilité d'avoir un canal privé pour faire des live sur des sites comme Twitch et Youtube.
+                        Si votre présence est fréquente vous avez la possibilité d'avoir un canal privé pour vous et vos amis.
+                        Notre communauté est entièrement gratuite et sans but lucratif.</p>
+
+                    <ul>
+                        <li><a href="http://www.clantemplates.com/video/How_to_DL_and_Edit/How_to_DL_and_Edit.html">How
+                                to
+                                Download &amp; Edit Your Template</a></li>
+                        <li>
+                            <a href="http://www.clantemplates.com/video/CT_How_to_Make_a_New_Page/CT_How_to_Make_a_New_Page.html">How
+                                to make a new page for the template</a></li>
+                        <li><a href="http://www.clantemplates.com/video/CT_How_to_Upload/CT_How_to_Upload.html">How to
+                                upload your template to a server</a></li>
+                    </ul>
+
+                    <h3>Editing your Template</h3>
+
+                    <p>All templates released at Clantemplates should include blank banner images for you to add your
+                        own
+                        clans name to in order to personalize the template. Such images are generally included in the
+                        images
+                        folder which was in the zip file you downloaded and are normally denoted by the word 'blank' in
+                        the
+                        filename. Other blank images for various parts of a template, may be included in the folder,
+                        depending on the template you've downloaded.</p>
+
+                    <h3>Getting Help</h3>
+
+                    <p>Any questions that the video tutorials just didn't cover? Well the best place to get the kind of
+                        answers you need is to use that template you've downloaded is to submit your question for the
+                        Support Staff in the Clantemplates <a
+                            href="http://forums.clantemplates.com/forumdisplay.php?f=9">Help
+                            Booth</a>. Make sure you include the name of the template you downloaded and the category it
+                        was
+                        in. Any question you may have regarding a template they will be able to answer for you.</p>
+                    <!-- / INSERT MAIN CONTENT HERE -->
+                    <img src="images/main_cont_bg_bottom.gif" alt=""/>
+                </div>
+            </div>
+            <!-- OP:EDIT FLAG 2 -->
+
+            <!-- END OP:EDIT FLAG 2 -->
+        </div>
+        <!-- / OP:MIDDLE COLUMN -->
+
+        <?php
+        include 'sections/right.php';
+        ?>
+    </div>
+    <!-- / OP:CONTENT -->
+
+<?php
+include 'sections/footer.php';
+?>
